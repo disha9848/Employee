3f5c2f21ca721f3b89278bb806624c911d1ff617
disha9848 <68861635+disha9848@users.noreply.github.com>
2021-03-24 15:13:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,43 +12,43 @@ module.exports.getEmployees = async (event, context, callback) => {
 };
 
 module.exports.getEmployeesByName = async (event, context, callback) => {
-    const users= await methods.getUserByName(req.body.name);
+    const users= await methods.getUserByName(event.body.name);
     callback(null,users);
   
 };
 
 module.exports.getEmployeesById = async (event, context, callback) => {
-    const user= await methods.getUserById(req.params.id);
+    const user= await methods.getUserById(event.params.id);
     callback(null,users);
   
 };
 
 module.exports.EmployeeLogin = async (event, context, callback) => {
-    const response= await methods.userLogin(req.body.password,req.body.email);
+    const response= await methods.userLogin(event.body.password,event.body.email);
     callback(null,users);
   
 };
 
 module.exports.addEmployee = async (event, context, callback) => {
-    const response= await methods.addUser(req.body.username,req.body.role,req.body.email, req.body.password,req.token);
+    const response= await methods.addUser(event.body.username,event.body.role,event.body.email, event.body.password,event.token);
     callback(null,users);
   
 };
 
 module.exports.addEmployeeAddress = async (event, context, callback) => {
-    const response= await methods.addAddress(req.body.employeeDetailId,req.body.address1,req.body.city, req.body.state,req.body.country,req.body.pincode);
+    const response= await methods.addAddress(event.body.employeeDetailId,event.body.address1,event.body.city, event.body.state,event.body.country,event.body.pincode);
     callback(null,users);
   
 };
 
 module.exports.updateEmployee = async (event, context, callback) => {
-    const user =await methods.updateUser(req.params.id,req.body.username, req.body.email,req.token);
+    const user =await methods.updateUser(event.params.id,event.body.username, event.body.email,event.token);
     callback(null,users);
   
 };
 
 module.exports.deleteEmployee = async (event, context, callback) => {
-    const user =await methods.deleteUser(req.params.id,req.token);
+    const user =await methods.deleteUser(event.params.id,event.token);
     callback(null,users);
   
 };
@@ -59,7 +59,7 @@ module.exports.verifyToken = async (event, context, callback) =>{
     if(typeof bearerHeader !== 'undefined'){
         const bearer=bearerHeader.split(' ');
         const bearerToken= bearer[1];
-        req.token=bearerToken;
+        event.token=bearerToken;
         next();
     }else{
         callback(null,sendStatus(403))
