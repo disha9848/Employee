7fbe3260e1453fe8c00c8373cc96404d01aa4839
disha9848 <68861635+disha9848@users.noreply.github.com>
2021-04-21 16:36:42 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,8 +1,20 @@
 const { Sequelize } = require('sequelize');
-const sequelize= new Sequelize('employeeDetails','user','password',{
-    dialect: 'sqlite',
-    host: './dev.sqlite'
-});
 
-
+const sequelize = new Sequelize(
+    "employees"  ,
+    "localhost",
+    "123556", {
+    host: "localhost",
+    port: 5432,
+    dialect: 'postgres',
+}
+);
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
 module.exports = sequelize;
